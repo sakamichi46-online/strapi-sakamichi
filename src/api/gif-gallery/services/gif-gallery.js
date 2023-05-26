e9a90bb784c5a95d1c008af3a3f64fdc98fdbd94
dwissaaj <dwisetyoaji@jrdataanalyst.com>
2023-05-26 10:36:17 +0700

    add api for sakamichi

---
+++ src/api/gif-gallery/services/gif-gallery.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gif-gallery service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::gif-gallery.gif-gallery');
